8b5b7937f06c24c933d574b9af64f7bc2b01ac7b
Andres <aflopez970@misena.edu.co>
2023-04-18 12:18:47 -0500

    actualizado

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Autos',
+        'USER':'postgres',
+        'PASSWORD':'LAcasona',
+        'HOST':'127.0.0.1',
+        'DATABASE_PORT':'5432',
     }
 }
 
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'
 
 TIME_ZONE = 'UTC'
 
@@ -122,6 +126,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
  os.path.join(BASE_DIR, 'static')
 ]
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from store.models import Customer, Product
+
+admin.site.register(Customer)
+admin.site.register(Product)
 
 # Register your models here.
