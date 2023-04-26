4e442c722741a03d71b6d0d42515a34a7c935657
andres felipe <aflopez970@misena.edu.co>
2023-04-26 16:09:39 -0500

    actu

--- store/views.py
+++ store/views.py
@@ -24,9 +24,9 @@ def registrar(request):
 
 def logins(request):
     if request.method== 'POST':
-        username=request.POST.get('username')
+        email=request.POST.get('email')
         password=request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=email, password=password)
         if user is not None:
             login(request, user)
             return redirect('store')
